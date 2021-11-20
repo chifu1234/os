ba50ae9094454b4f67b91a6e99b9da3598dc665a
chifu <chifu@localhost.localdomain>
2021-11-20 14:07:25 +0100

    init

--- .scripts/repo.sh
+++ .scripts/repo.sh
@@ -2,7 +2,6 @@
 # Disable stdin
 exec 0<&- 
 export PATH=~/depot_tools:$PATH 
-mkdir ~/chromiumos 
 cd ~/chromiumos 
 repo init -u https://chromium.googlesource.com/chromiumos/manifest.git --repo-url https://chromium.googlesource.com/external/repo.git
 repo sync -j16 
