24a617b4f84b390e08dfd04868df2457066c3958
chifu <chifu@localhost.localdomain>
2021-11-20 14:28:07 +0100

    init

--- .scripts/dep.sh
+++ .scripts/dep.sh
@@ -13,4 +13,4 @@ fi
 echo "🎉 Lets start by getting all deps"
 ${SUDO} apt update 
 ${SUDO} apt-get install -y git-core curl lvm2 thin-provisioning-tools python-pkg-resources python3-virtualenv python3-oauth2client xz-utils python3.6 sudo 
-[ ! -d ~/depot_tools ] &&  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git ~/depot_tools 
+[ ! -d "./depot_tools" ] &&  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git ~/depot_tools 
