84dc5b3e6fd289b293ea508d4140df60fbd2ad5f
chifu <chifu@localhost.localdomain>
2021-11-20 10:44:23 +0100

    init

--- .scripts/ci.sh
+++ .scripts/ci.sh
@@ -11,15 +11,15 @@ then
 fi
 
 echo "🎉 Lets start by getting all deps"
-${SUDO} apt update  >/dev/null
-${SUDO} apt-get install -y git-core curl lvm2 thin-provisioning-tools python-pkg-resources python3-virtualenv python3-oauth2client xz-utils python3.6 sudo >/dev/null
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git ~/depot_tools >/dev/null
+${SUDO} apt update 
+${SUDO} apt-get install -y git-core curl lvm2 thin-provisioning-tools python-pkg-resources python3-virtualenv python3-oauth2client xz-utils python3.6 sudo 
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git ~/depot_tools 
 export PATH=~/depot_tools:$PATH 
 echo "🐧 Now get the sources (around 30min)"
-mkdir ~/chromiumos >/dev/null
-cd ~/chromiumos >/dev/null
-repo init -u https://chromium.googleso urce.com/chromiumos/manifest.git --repo-url https://chromium.googlesource.com/external/repo.git >/dev/null
-repo sync -j16 >/dev/null
+mkdir ~/chromiumos 
+cd ~/chromiumos 
+repo init -u https://chromium.googleso urce.com/chromiumos/manifest.git --repo-url https://chromium.googlesource.com/external/repo.git 
+repo sync -j16 
 echo "🔎 Now let's build chromium os (1h)."
 cros_sdk
 cros_sdk setup_board --board=amd64-generic
